4cba3313789b279ba270a61b7f32fecba70bd64a
alxc89 <alexcaardoso@gmail.com>
2020-11-01 13:26:18 -0300

    Aprovação

---
+++ src/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  return res.sendFile(__dirname + "/views/index.html");
+});
+
+app.listen(8008);
